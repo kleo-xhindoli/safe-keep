a30802270683b9df3187e5523d18f29ca7f19dfa
kleo <s-kxhindoli@subsplash.com>
2020-10-25 12:53:32 +0100

    read data from firebase

---
+++ src/hooks/resources/secrets.ts
@@ -0,0 +1,12 @@
+import * as firebase from "firebase";
+import { useListVals } from "react-firebase-hooks/database";
+import { Secret } from "../../types/secret";
+
+export function useSecrets(safeId: string) {
+  const secretsRef = firebase.database().ref(`safes/${safeId}/secrets`);
+  const [safes, loading, error] = useListVals<Secret>(secretsRef, {
+    keyField: "id",
+  });
+
+  return [safes, loading, error] as const;
+}
