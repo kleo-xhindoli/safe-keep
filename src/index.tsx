3a021941b617e6bf32f427c4d6d39425e784f5b2
kleo <s-kxhindoli@subsplash.com>
2020-10-25 11:32:43 +0100

    add firebase config

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import config, { env } from "./config";
+import * as firebase from "firebase";
+
+firebase.initializeApp(config.firebase);
+
+if (env === "local") {
+  window.firebase = firebase;
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
